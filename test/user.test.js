0bad8cf537ec3ff1046b17aabcf57d9493b27ddf
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-10-05 18:30:52 +0100

    Added more tests

--- test/user.test.js
+++ test/user.test.js
@@ -1,23 +1,44 @@
 const request = require("supertest");
-const User = require("../src/models/User");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 
-const { CreateUser } = require("../src/utils/user");
+dotenv.config();
+
+const { default: User } = require("../src/models/User");
+
+const { CreateUser, EditUser } = require("../src/utils/user");
+const { describe } = require("node:test");
+
 
 jest.mock("../src/models/User");
+jest.mock("bcrypt");
+
+bcrypt.genSalt.mockImplementation(() => jest.fn().mockResolvedValue(Math.ceil(Math.random() * 1000))());
+bcrypt.hash.mockImplementation((password, salt) => jest.fn().mockResolvedValue(password + salt)());
+
 
 describe("Testing 'CreateUser' class", () => {
     const USER_DETAILS = {
         name: "Ezema Emmanuel",
         email: "emmanuelezema6@gmail.com",
         username: "ennanuel",
-        password: "TheFlash!3",
-        confirmPassword: "TheFlash!3"
+        password: "SuperMan!3",
+        confirmPassword: "SuperMan!3"
     }
 
     it("Creates a new user class", async () => {
-        User.create.mockImplementation((values) => jest.fn().mockResolvedValue({ _id: Date.now(), _doc: { _id: Date.now(), ...values } }));
-        User.countDocuments.mockImplementation((filters) => jest.fn().mockResolvedValue(0));
-        
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(0)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+
         const newUser = new CreateUser(USER_DETAILS);
         const validation = await newUser.validate();
         const savedUser = await newUser.save();
@@ -28,10 +49,31 @@ describe("Testing 'CreateUser' class", () => {
             email: USER_DETAILS.email,
             username: USER_DETAILS.username
         });
-        expect(savedUser).toEqual({ failed: false, message: "New user saved" });
+        expect(savedUser).toEqual({
+            failed: false,
+            message: "New user created",
+            userDetails: {
+                _id: "user123",
+                isAdmin: false
+            }
+        });
+        expect(bcrypt.genSalt).toHaveBeenCalledTimes(1);
+        expect(bcrypt.hash).toHaveBeenCalledTimes(1);
     });
 
     it("Validate user details", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(0)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: 'user123',
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+
         const newUser = new CreateUser();
 
         const validation = await newUser.validate();
@@ -40,79 +82,135 @@ describe("Testing 'CreateUser' class", () => {
         expect(validation).toEqual({
             passed: false,
             errors: {
-                name: "User must have a name",
                 email: "User must have an email",
                 username: "User must have a username",
-                password: "Password must be at least 8 characters long",
+                password: "Password must be at least 8 characters long"
             }
         });
         expect(savedUser).toEqual({
             failed: true,
             message: "Validation error",
             errors: {
-                name: "User must have a name",
                 email: "User must have an email",
                 username: "User must have a username",
-                password: "Password must be at least 8 characters long",
+                password: "Password must be at least 8 characters long"
             }
         });
     });
 
-    it("Validates if email and username already exists", async () => {
-        const mockUsers = [];
+})
 
-        User.countDocuments.mockImplementation(({ [field]: value }) => jest
-            .fn()
-            .mockResolvedValue(
-                mockUsers.filter(user => user[field] === value).length
-            )
-        );
-        User.create.mockImplementation((values) => {
-            mockUsers.push(values);
-            return jest.fn().mockResolvedValue({
-                _id: Date.now(),
-                _doc: {
-                    _id: Date.now(),
-                    ...values
-                }
-            });
-        });
 
-        const newUser = new CreateUser(USER_DETAILS);
-        const validation1 = await newUser.validate();
-        const savedUser1 = await newUser.save();
+describe("Name, username and email validation", () => {
 
-        const newUser2 = new CreateUser({ ...USER_DETAILS, name: "John Stones" });
-        const validation2 = await newUser2.validate();
-
-        expect(validation1).toEqual({
-            passed: true,
-            errors: {}
-        });
-        expect(savedUser1).toEqual({
-            failed: false,
-            message: "New user saved"
-        });
-        expect(validation2).toEqual({
-            passed: false,
-            errors: {
-                email: "Email already exists",
-                username: "Username already exists"
-            }
-        });
-    })
-});
-
-describe("Testing password verification", () => {
     const USER_DETAILS = {
-        name: "Emmanuel Ezema",
+        name: "Ezema Emmanuel",
         email: "emmanuelezema6@gmail.com",
         username: "ennanuel",
         password: "TheFlash!3",
         confirmPassword: "TheFlash!3"
+    }
+
+    it("Validates if email and username already exists", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(1)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+
+        const newUser = new CreateUser({ name: "", ...USER_DETAILS });
+        const validation = await newUser.validate();
+        const savedUser = await newUser.save();
+        
+        expect(validation).toEqual({
+            passed: false,
+            errors: {
+                email: "email already exists",
+                username: "username already exists"
+            }
+        });
+        expect(savedUser).toEqual({
+            failed: true,
+            message: "Validation error",
+            errors: {
+                email: "email already exists",
+                username: "username already exists"
+            }
+        });
+
+    })
+
+    it("Validates if name, email or username is invalid", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(1)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+
+        const newUser = new CreateUser({ 
+            ...USER_DETAILS, 
+            name: "Bad Name1", 
+            email: "this_is_not_an_email.com", 
+            username: "Invalid Username"
+        });
+        const validation = await newUser.validate();
+        const savedUser = await newUser.save();
+
+        expect(validation).toEqual({
+            passed: false,
+            errors: {
+                name: "User's name must contain only letters",
+                email: "Please use a valid email",
+                username: "username cannot have charaters like: white spaces, '*', '+', '.', '&', '(', or ')'"
+            }
+        })
+        expect(savedUser).toEqual({
+            failed: true,
+            message: "Validation error",
+            errors: {
+                name: "User's name must contain only letters",
+                email: "Please use a valid email",
+                username: "username cannot have charaters like: white spaces, '*', '+', '.', '&', '(', or ')'"
+            }
+        })
+    })
+});
+
+describe("Testing password verification", () => {
+
+    const USER_DETAILS = {
+        name: "Emmanuel Ezema",
+        email: "emmanuelezema6@gmail.com",
+        username: "ennanuel",
+        password: "SuperMan!3",
+        confirmPassword: "SuperMan!3"
     };
     
     it("Should check password length", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(0)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+
         const newUser = new CreateUser({ ...USER_DETAILS, password: "abc123" });
         const validation = await newUser.validate();
 
@@ -126,6 +224,18 @@ describe("Testing password verification", () => {
     });
 
     it("Should check for lowercase alphabets in password", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(0)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+        
         const newUser = new CreateUser({ ...USER_DETAILS, password: "1234ABCD" });
         const validation = await newUser.validate();
 
@@ -139,6 +249,18 @@ describe("Testing password verification", () => {
     });
 
     it("Should check for uppercase letters in password", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(0)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+        
         const newUser = new CreateUser({ ...USER_DETAILS, password: "1234abcd" });
         const validation = await newUser.validate();
 
@@ -152,6 +274,18 @@ describe("Testing password verification", () => {
     });
 
     it("Should check for numbers in password", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(0)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+        
         const newUser = new CreateUser({ ...USER_DETAILS, password: "abcdEFGH" });
         const validation = await newUser.validate();
 
@@ -165,6 +299,18 @@ describe("Testing password verification", () => {
     });
 
     it("Should check for symbols in password", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(0)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+        
         const newUser = new CreateUser({ ...USER_DETAILS, password: "abcDEF12" });
         const validation = await newUser.validate();
 
@@ -178,6 +324,18 @@ describe("Testing password verification", () => {
     });
 
     it("Should check if password and password confirmation match", async () => {
+        User.countDocuments.mockImplementation(() => jest.fn().mockResolvedValue(0)());
+        User.mockImplementation((values) => ({
+            save: jest.fn().mockResolvedValue({
+                _id: "user123",
+                _doc: {
+                    _id:
+                        "user123",
+                    ...values
+                }
+            })
+        }));
+        
         const newUser = new CreateUser({ ...USER_DETAILS, password: "abcDEF12$" });
         const validation = await newUser.validate();
 
@@ -190,17 +348,97 @@ describe("Testing password verification", () => {
     });
 });
 
+describe("Testing user details editing class", () => {
+    const USER_DETAILS = {
+        id: 1234,
+        name: "Ezema Emmanuel",
+        email: "emmanuelezema6@gmail.com",
+        username: "ennanuel"
+    };
 
-// describe("Testing user registration route");
+    const USER_FIND_RESULT = {
+        id: 1234,
+        name: "Nnanna Emmanuel",
+        email: "emmanuelezema2@gmail.com",
+        username: "ennanuel1"
+    }
 
+    it("Validates the values to update", async () => {
+        User.countDocuments.mockImplementation((values) => jest.fn().mockResolvedValue(0)());
+        User.findById.mockImplementation(() => ({ lean: jest.fn().mockResolvedValue(USER_FIND_RESULT) }));
+        User.findByIdAndUpdate.mockImplementation((values) => jest.fn().mockResolvedValue(values)());
+        
+        const user = new EditUser(USER_DETAILS);
+        const validation = await user.validate();
+        const updatedUser = await user.save();
 
-// describe("Testing user login route");
+        expect(validation.passed).toBe(true);
+        expect(validation.errors).toEqual({});
+        expect(updatedUser).toEqual({
+            failed: false,
+            message: "User details updated"
+        })
+    });
 
+    it("Validate 'EditUser' class initialized without details", async () => { 
+        User.countDocuments.mockImplementation((values) => jest.fn().mockResolvedValue(0)());
+        User.findById.mockImplementation(() => ({ lean: jest.fn().mockResolvedValue(null) }));
+        
+        const user = new EditUser();
+        const validation = await user.validate();
+        const savedUser = await user.save();
 
-// describe("Testing user authentication route");
+        expect(validation.passed).toBe(false);
+        expect(validation.errors).toEqual({ user: "User not found" });
+        expect(savedUser.failed).toBe(true);
+        expect(savedUser.message).toBe("Validation error");
+        expect(savedUser.errors).toEqual({ user: "User not found" });
+    })
 
+    it("Validation for invalid email and username", async () => {
+        User.countDocuments.mockImplementation((values) => jest.fn().mockResolvedValue(0)());
+        User.findById.mockImplementation(() => ({ lean: jest.fn().mockResolvedValue(USER_FIND_RESULT) }));
+        
+        const user = new EditUser({ ...USER_DETAILS, email: "not_an_email.com", username: "bad username" });
+        const validation = await user.validate();
 
-// describe("Testing user details update route");
+        expect(validation.passed).toBe(false);
+        expect(validation.errors).toEqual({
+            email: "New email isn't valid",
+            username: "New username cannot have charaters like: white spaces, '*', '+', '.', '&', '(', or ')'"
+        })
+    })
 
+    it("Checks if email and username already exists", async () => {
+        User.countDocuments.mockImplementation((values) => jest.fn().mockResolvedValue(1)());
+        User.findById.mockImplementation(() => ({ lean: jest.fn().mockResolvedValue(USER_FIND_RESULT) }));
 
-// describe("Testing user delete route");
+        const user = new EditUser(USER_DETAILS);
+        const validation = await user.validate();
+
+        expect(validation.passed).toBe(false);
+        expect(validation.errors).toEqual({
+            email: "Email already exists",
+            username: "Username already exists"
+        })
+    })
+
+    it("Checks if new values are the same with the previous values", async () => {
+        User.countDocuments.mockImplementation((values) => jest.fn().mockResolvedValue(0)());
+        User.findById.mockImplementation(() => ({ lean: jest.fn().mockResolvedValue(USER_DETAILS) }));
+
+        const user = new EditUser(USER_DETAILS);
+        const validation = await user.validate();
+        const savedUser = await user.save();
+
+        expect(validation.passed).toBe(false);
+        expect(validation.errors).toEqual({
+            user: "There is no change in the new values"
+        });
+        expect(savedUser.failed).toBe(true);
+        expect(savedUser.message).toBe("Validation error");
+        expect(savedUser.errors).toEqual({
+            user: "There is no change in the new values"
+        });
+    })
+})
