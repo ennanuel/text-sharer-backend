f0006f86ef496e22d8786493a63ddb84ea1a7158
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-08 11:47:34 +0100

    modified functions, and removed some redundant functions

--- src_input/controllers/space.ts
+++ src_input/controllers/space.ts
@@ -1,14 +1,15 @@
 import { RequestHandler } from "express";
 import { handleError, ModifiedError } from "../utils/error";
-import { CreateTextSpace, deleteTextAndEditUserDetailsSpace, EditTextSpace, getSingleSecuredSpace, getSingleUnsecuredSpace, getSpacesOfOtherUsers, getUserSpaces } from "../utils/spaces";
+import { CreateTextSpace, deleteTextAndEditUserDetailsSpace, EditTextSpace, getSingleSpace, getSpacesOfOtherUsers, getUserSpaces } from "../utils/spaces";
 import { ModifiedRequest } from "../utils/auth";
+import { io } from "../server";
 
 
 export const getUserTextSpaces: RequestHandler = async (req, res) => {
     try {
-
-        const { userId, page } = req.params;
-        const { limit, filter } = req.query;
+        const userId = (req as ModifiedRequest)?.auth?.id;
+        const { page } = req.params;
+        const { limit = 9, filter } = req.query;
         const options = { page, limit, filter };
 
         const result = await getUserSpaces(userId, options);
@@ -25,8 +26,9 @@ export const getUserTextSpaces: RequestHandler = async (req, res) => {
 
 export const exploreTextSpaces: RequestHandler = async (req, res) => { 
     try {
-        const { userId, page } = req.params;
-        const { limit, sortBy } = req.query;
+        const userId = (req as ModifiedRequest)?.auth?.id;
+        const { page } = req.params;
+        const { limit = 9, sortBy } = req.query;
         const options = { page, limit, sortBy };
 
         const result = await getSpacesOfOtherUsers(userId, options);
@@ -41,28 +43,15 @@ export const exploreTextSpaces: RequestHandler = async (req, res) => {
     }
 };
 
-export const getSingleUnsecuredTextSpace: RequestHandler = async (req, res) => { 
+export const getSingleTextSpace: RequestHandler = async (req, res) => { 
     try {
+        const userId = (req as ModifiedRequest)?.auth?.id
         const { textSpaceId } = req.params;
-        const result = await getSingleUnsecuredSpace(textSpaceId);
-        if (result.failed) throw result;
+        const { p = "" } = req.query;
+        const { failed, textSpace, ...result } = await getSingleSpace(textSpaceId, p, userId);
+        if (failed) throw result;
         
-        return res.status(200).json(result);
-    } catch (error) {
-        console.error(error);
-        const { statusCode, ...result } = handleError((error as ModifiedError));
-        return res.status(statusCode).json(result);
-    }
-};
-
-export const getSingleSecuredTextSpace: RequestHandler = async (req, res) => { 
-    try {
-        const { textSpaceId } = req.params;
-        const { p } = req.query;
-        const result = await getSingleSecuredSpace(textSpaceId, p);
-        if (result.failed) throw result;
-        
-        return res.status(200).json(result);
+        return res.status(200).json(textSpace);
     } catch (error) {
         console.error(error);
         const { statusCode, ...result } = handleError((error as ModifiedError));
@@ -72,13 +61,16 @@ export const getSingleSecuredTextSpace: RequestHandler = async (req, res) => {
 
 export const create: RequestHandler = async (req, res) => { 
     try {
-        const { title, desc, content, secured, password, owner } = req.body;
+        const { title, desc, content, secured, password, color } = req.body;
+        const owner = (req as ModifiedRequest)?.auth?.id;
 
-        const newTextSpace = new CreateTextSpace({ title, desc, content, secured, password, owner });
+        const newTextSpace = new CreateTextSpace({ title, desc, content, secured, password, owner, color });
         await newTextSpace.validate();
         const result = await newTextSpace.save();
 
         if (result.failed) throw result;
+
+        io.emit('created', { userId: owner });
         return res.status(200).json(result);
     } catch (error) {
         console.error(error);
@@ -89,13 +81,16 @@ export const create: RequestHandler = async (req, res) => {
 
 export const edit: RequestHandler = async (req, res) => {
     try {
+        const userId = (req as ModifiedRequest)?.auth?.id;
         const { textSpaceId } = req.params;
-        const { title, desc, content, secured, password } = req.body;
-        const edittedTextSpace = new EditTextSpace({ textSpaceId, title, desc, content, secured, password });
+        const { title, desc, content, color, secured, password } = req.body;
+        const edittedTextSpace = new EditTextSpace({ textSpaceId, title, desc, content, secured, password, color });
         await edittedTextSpace.validate();
         const result = await edittedTextSpace.save();
 
         if (result.failed) throw result;
+
+        io.emit('editted', { textSpaceId, userId });
         return res.status(200).json(result);
     } catch (error) {
         console.error(error);
@@ -106,11 +101,13 @@ export const edit: RequestHandler = async (req, res) => {
 
 export const deleteTextSpace: RequestHandler = async (req, res) => {
     try {
+        const userId = (req as ModifiedRequest)?.auth?.id
         const { textSpaceId } = req.params;
         const result = await deleteTextAndEditUserDetailsSpace(textSpaceId);
 
         if (result.failed) throw result;
 
+        io.emit('deleted', { textSpaceId, userId });
         return res.status(200).json(result);
     } catch (error) {
         console.error(error);
