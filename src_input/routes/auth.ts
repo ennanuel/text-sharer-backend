a10ffa601dd94004eb655f995c0849ab2413625f
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-08 11:45:35 +0100

    modified some routes

--- src_input/routes/auth.ts
+++ src_input/routes/auth.ts
@@ -1,10 +1,11 @@
 
 import { Router } from "express";
-import { authenticate, checkToken } from "../utils/auth";
+import { authenticate, checkToken, invalidateToken } from '../utils/auth';
 
 
 const route = Router();
 
 route.get("/check", authenticate, checkToken);
+route.post("/logout", invalidateToken);
 
 export default route;
