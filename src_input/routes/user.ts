764e85f28b6071037e7134e8967e9aecccaa1bf3
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-08 11:43:54 +0100

    Removed some routes

--- src_input/routes/user.ts
+++ src_input/routes/user.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { register, login, edit, deleteUser, getUser, logout } from "../controllers/user";
+import { register, login, edit, deleteUser, getUser } from "../controllers/user";
 
 const route = Router();
 
 route.get("/details/:userId", getUser)
 route.post('/register', register);
 route.post('/login', login);
-route.post('/logout', logout);
 route.put('/edit/:userId', edit);
 route.delete('/delete/:userId', deleteUser);
 
