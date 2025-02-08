0d04b22a95f35b35f6747baead9f9c9f9606882e
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-02-08 11:44:31 +0100

    added middleware functions for routes, and removed some unused routes

--- src_input/routes/space.ts
+++ src_input/routes/space.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { create, edit, deleteTextSpace, getSingleUnsecuredTextSpace, getSingleSecuredTextSpace, getUserTextSpaces, exploreTextSpaces } from "../controllers/space"
+import { create, edit, deleteTextSpace, getSingleTextSpace, getUserTextSpaces, exploreTextSpaces } from "../controllers/space"
+import { authenticate, authenticateWithoutKickingout } from '../utils/auth';
 
 const route = Router();
 
-route.get("/user/:userId/:page", getUserTextSpaces);
-route.get("/explore/:page/:userId?", exploreTextSpaces);
-route.get("/space/unsecured/:textSpaceId", getSingleUnsecuredTextSpace);
-route.get("/space/secured/:textSpaceId", getSingleSecuredTextSpace);
-route.post("/create", create);
-route.put("/edit/:textSpaceId", edit);
-route.delete("/delete/:textSpaceId", deleteTextSpace);
+route.get("/user/:page", authenticate, getUserTextSpaces);
+route.get("/explore/:page", authenticateWithoutKickingout, exploreTextSpaces);
+route.get("/space/:textSpaceId", authenticateWithoutKickingout, getSingleTextSpace);
+route.post("/create", authenticateWithoutKickingout, create);
+route.put("/edit/:textSpaceId", authenticate, edit);
+route.delete("/delete/:textSpaceId", authenticate, deleteTextSpace);
 
 export default route
